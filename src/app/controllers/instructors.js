59aa941d645cc5a019c533b74df07f2d94989d70
Marcos Carvalho <63380367+MVGC1989@users.noreply.github.com>
2021-02-03 14:32:56 -0300

    Gym Final

--- src/app/controllers/instructors.js
+++ src/app/controllers/instructors.js
@@ -16,11 +16,12 @@ module.exports = {
             limit,
             offset,
             callback(instructors){
-
+                
                 const pagination = { 
                     total: Math.ceil(instructors[0].total / limit),
                     page
                 }
+                
                 return res.render("instructors/index" , {instructors , pagination , filter})
             }
         }
